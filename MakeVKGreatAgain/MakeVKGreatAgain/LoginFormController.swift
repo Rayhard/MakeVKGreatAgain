3cc1f1998eab50d110e21fca3aa9aa9ff031ed7d
nperezhogin <nperezhogin@yandex.ru>
2020-02-08 11:25:20 +0300

    Интерфейс - Урок 1

---
+++ MakeVKGreatAgain/MakeVKGreatAgain/LoginFormController.swift
@@ -0,0 +1,79 @@
+//
+//  LoginFormController.swift
+//  MakeVKGreatAgain
+//
+//  Created by n.perezhogin on 08.02.2020.
+//  Copyright © 2020 n.perezhogin. All rights reserved.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBAction func singInButton(_ sender: Any) {
+        if let login = loginTextField.text,
+            let password = passwordTextField.text{
+            print("Login: \(login), Password: \(password)")
+            
+            if login == "admin", password == "admin"{
+                print("All ok")
+            } else {
+                print("Not ok")
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        scrollView.addGestureRecognizer(tapGesture)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: Notification){
+        let info = notification.userInfo! as NSDictionary
+        let size = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: size.height, right: 0)
+        self.scrollView?.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardWillHide(notification: Notification){
+        scrollView.contentInset = .zero
+    }
+    
+    @objc func hideKeyboard() {
+        self.scrollView.endEditing(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
