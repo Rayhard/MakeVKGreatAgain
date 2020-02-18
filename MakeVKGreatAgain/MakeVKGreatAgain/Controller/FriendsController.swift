3eb8c424ef926e7a218e4283cf5ad8956b8a8d8d
nperezhogin <nperezhogin@yandex.ru>
2020-02-18 10:35:15 +0300

    Домашняя работа - Урок 5

--- MakeVKGreatAgain/MakeVKGreatAgain/Controller/FriendsController.swift
+++ MakeVKGreatAgain/MakeVKGreatAgain/Controller/FriendsController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class FriendsController: UITableViewController {
     
-    var friendsArray: [User] = [User(name: "Nikita",
+    let friendsArray: [User] = [User(name: "Nikita",
                                      photo: UIImage(named: "Nikita"),
                                      images: [UIImage(named: "Nikita")!]),
                                 User(name: "Petya",
@@ -18,29 +18,81 @@ class FriendsController: UITableViewController {
                                      images: [UIImage(named: "Petya")!]),
                                 User(name: "Olga",
                                      photo: UIImage(named: "Olga"),
-                                     images: [UIImage(named: "Olga")!])
+                                     images: [UIImage(named: "Olga")!]),
+                                User(name: "Nikita1",
+                                     photo: UIImage(named: "Nikita"),
+                                     images: [UIImage(named: "Nikita")!]),
+                                User(name: "Petya1",
+                                     photo: UIImage(named: "Petya"),
+                                     images: [UIImage(named: "Petya")!]),
+                                User(name: "Olga1",
+                                     photo: UIImage(named: "Olga"),
+                                     images: [UIImage(named: "Olga")!]),
+                                User(name: "Nikita2",
+                                     photo: UIImage(named: "Nikita"),
+                                     images: [UIImage(named: "Nikita")!]),
+                                User(name: "Petya2",
+                                     photo: UIImage(named: "Petya"),
+                                     images: [UIImage(named: "Petya")!]),
+                                User(name: "Olga2",
+                                     photo: UIImage(named: "Olga"),
+                                     images: [UIImage(named: "Olga")!]),
+                                User(name: "Nikita3",
+                                     photo: UIImage(named: "Nikita"),
+                                     images: [UIImage(named: "Nikita")!]),
+                                User(name: "Petya3",
+                                     photo: UIImage(named: "Petya"),
+                                     images: [UIImage(named: "Petya")!]),
+                                User(name: "Olga3",
+                                     photo: UIImage(named: "Olga"),
+                                     images: [UIImage(named: "Olga")!]),
     ]
+    
+    var names: [User]{
+        return friendsArray.sorted(by: {$0.name < $1.name})
+    }
+    lazy var sections: [[User]] = {
+        return names.reduce([[User]]()){ (result, element) -> [[User]] in
+            guard var last = result.last else {return [[element]]}
+            var collection = result
+            if element.name.first == result.last?.first?.name.first{
+                last.append(element)
+                collection[collection.count-1] = last
+            } else {
+                collection.append([element])
+            }
+            return collection
+            
+        }
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
-    // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return sections.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return friendsArray.count
+        return sections[section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].first?.name.first?.uppercased()
+    }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return Array(Set(names.compactMap{$0.name.first?.uppercased()})).sorted()
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FriendCell
-        
-        cell.friendName.text = friendsArray[indexPath.row].name
-        cell.friendImage.image = friendsArray[indexPath.row].photo
+        let name = sections[indexPath.section][indexPath.row].name
+        let image = sections[indexPath.section][indexPath.row].photo
+        cell.friendName.text = name
+        cell.friendImage.image = image
 
         return cell
     }
@@ -48,45 +100,9 @@ class FriendsController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToFriendImages"{
             if let indexPath = tableView.indexPathForSelectedRow {
-                (segue.destination as? FriendsImagesController)?.friendImagesArray = friendsArray[indexPath.row].images
+                (segue.destination as? FriendsImagesController)?.friendImagesArray = sections[indexPath.section][indexPath.row].images
                 tableView.deselectRow(at: indexPath, animated: true)
             }
         }
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
 }
--- MakeVKGreatAgain/MakeVKGreatAgain/Controller/LoginFormController.swift
+++ MakeVKGreatAgain/MakeVKGreatAgain/Controller/LoginFormController.swift
@@ -86,15 +86,4 @@ class LoginFormController: UIViewController {
     @objc func hideKeyboard() {
         self.scrollView.endEditing(true)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
